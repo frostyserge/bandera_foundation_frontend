17399450402dc0abae3b0184d05446f041e21b02
frostyserge <eyemoroz@gmail.com>
2023-07-14 23:39:16 -0400

    initialized React app. Created necessary components for auth. installed and imported react bootstrap.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+// import Login from './components/Login';
+// import Home from './components/Home';
+import Navigation from './components/Navigation';
+// import Logout from './components/Logout';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navigation />
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+          {/* <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} /> */}
+        </Routes>
+      </Router>
     </div>
   );
 }

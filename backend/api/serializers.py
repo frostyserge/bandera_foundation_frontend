746ce8b02f232945f8c4e2f10b101c8649c2393d
frostyserge <eyemoroz@gmail.com>
2023-07-15 16:08:34 -0400

    removed backend

--- backend/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Merch
-
-class MerchSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Merch
-        fields = ('id', 'name', 'price', 'description', 'created_at')
-

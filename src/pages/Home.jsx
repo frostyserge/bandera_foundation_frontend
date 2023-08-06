2860d08a3c2cfe40c509f3dc55efeec74b1b9221
frostyserge <eyemoroz@gmail.com>
2023-08-05 23:25:29 -0400

    started reworking the homepage, working on making an APi call for Events that will be displayed in a carousel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,64 @@
-import { Carousel } from 'react-bootstrap';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 function Home() {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
     return (
-            <Carousel data-bs-theme="dark" style={{ height: '18rem'}}>
-              <Carousel.Item style={{ height: '18rem', margin: '10px'}}>
-              <h2 className="text-center">
-                  Event 1
-                </h2>
-                <Carousel.Caption>
-                  <h5>Event Name</h5>
-                  <p>Event Description</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <h2 className="text-center">
-                  Event 2
-                </h2>
-                <Carousel.Caption>
-                  <h5>Event Name</h5>
-                  <p>Event Description</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-              <h2 className="text-center">
-                  Event 3
-                </h2>
-                <Carousel.Caption>
-                  <h5>Event Name</h5>
-                  <p>
-                  Event Name.
-                  </p>
-                </Carousel.Caption>
-              </Carousel.Item>
-            </Carousel>
+      <Carousel responsive={responsive}>
+      <div>Item 1</div>
+      <div>Item 2</div>
+      <div>Item 3</div>
+      <div>Item 4</div>
+      </Carousel>
+            // <Carousel responsive={responsive} data-bs-theme="dark" style={{ height: '18rem'}}>
+            //   <Carousel.Item style={{ height: '18rem', margin: '10px'}}>
+            //   <h2 className="text-center">
+            //       Event 1
+            //     </h2>
+            //     <Carousel.Caption>
+            //       <h5>Event Name</h5>
+            //       <p>Event Description</p>
+            //     </Carousel.Caption>
+            //   </Carousel.Item>
+            //   <Carousel.Item>
+            //     <h2 className="text-center">
+            //       Event 2
+            //     </h2>
+            //     <Carousel.Caption>
+            //       <h5>Event Name</h5>
+            //       <p>Event Description</p>
+            //     </Carousel.Caption>
+            //   </Carousel.Item>
+            //   <Carousel.Item>
+            //   <h2 className="text-center">
+            //       Event 3
+            //     </h2>
+            //     <Carousel.Caption>
+            //       <h5>Event Name</h5>
+            //       <p>
+            //       Event Name.
+            //       </p>
+            //     </Carousel.Caption>
+            //   </Carousel.Item>
+            // </Carousel>
     );
 }
 
